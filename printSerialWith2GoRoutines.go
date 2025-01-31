b1061050fa04cce0d2dd237771dab335bb1d7bdc
Syed Ahmed Mohiuddin Peerzade <syed.ahm.mohiuddin-peerzade@hpe.com>
2025-01-31 20:02:10 +0530

    Update printSerialWith2GoRoutines.go

--- printSerialWith2GoRoutines.go
+++ printSerialWith2GoRoutines.go
@@ -5,33 +5,37 @@ import (
 	"sync"
 )
 
-func printEven(input int, C chan bool, wg *sync.WaitGroup) {
+func printEven(input int, C chan bool) {
 	for i := 2; i <= input; i += 2 {
 		<-C
 		fmt.Println(i)
 		C <- true
 	}
-	wg.Done()
 
 }
 
-func printOdd(input int, C chan bool, wg *sync.WaitGroup) {
+func printOdd(input int, C chan bool) {
 	for i := 1; i <= input; i += 2 {
 		fmt.Println(i)
 		C <- true
 		<-C
 	}
 
-	wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
 	C := make(chan bool)
 	wg.Add(1)
-	go printEven(100, C, &wg)
+	go func() {
+		printEven(100, C)
+		wg.Done()
+	}()
 	wg.Add(1)
-	go printOdd(100, C, &wg)
+	go func() {
+		printOdd(100, C)
+		wg.Done()
+	}()
 	wg.Wait()
 
 }
