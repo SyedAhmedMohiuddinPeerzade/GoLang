69629256a7b345b75a57ef113936952b65716083
Syed Ahmed Mohiuddin Peerzade <syed.ahm.mohiuddin-peerzade@hpe.com>
2025-02-14 16:54:51 +0530

    Update VariadicExample.go

--- VariadicExample.go
+++ VariadicExample.go
@@ -1,1 +1,39 @@
 
+package main
+
+import "fmt"
+
+// Sum returns the sum of adding all the numbers in
+// the parameters together.
+func Sum(numbers ...int) int {
+
+    n := 0
+
+    for _,number := range numbers {
+        n += number
+    }
+
+    return n
+}
+
+func main() {
+    sm1 := Sum(1, 2, 3, 4) // = 1 + 2 + 3 + 4 = 10
+
+    sm2 := Sum(1, 2) // = 1 + 2 = 3
+
+    sm3 := Sum(7, 1, -2, 0, 18) // = 7 + 1 + -2 + 0 + 18 = 24
+
+    sm4 := Sum() // = 0
+
+
+    fmt.Printf("sm1 = %d\n", sm1)
+    fmt.Printf("sm2 = %d\n", sm2)
+    fmt.Printf("sm3 = %d\n", sm3)
+    fmt.Printf("sm4 = %d\n", sm4)
+
+    // Output:
+    // sm1 = 10
+    // sm2 = 3
+    // sm3 = 24
+    // sm4 = 0
+}
